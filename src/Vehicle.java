dccadd3bfbaee1b83778c0a557b7d76a7a013bcb
Jun-ho, Song <jun9358@gmail.com>
2013-06-07 00:59:10 +0900

    Add moving vehicle
    It move 1 pixel by one time.

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,8 +1,22 @@
 import java.awt.Graphics;
-
+import java.awt.Point;
 
 public interface Vehicle
-{	
+{
+	public static final Point dir[] =
+	{
+		/* {x, y} */
+		new Point(01, 00),	// East
+		new Point(-1, 00),	// West
+		new Point(00, +1),	// South
+		new Point(00, -1)	// North
+	};
+	public static final int DIR_EAST = 0;
+	public static final int DIR_WSET = 1;
+	public static final int DIR_SOUTH = 2;
+	public static final int DIR_NORTH = 3;
+	
 	public void paintComponent(Graphics g);
 	public void setBounds(int x, int y, int width, int height);
+	public void move(int direction);
 }
--- src/VehicleCar.java
+++ src/VehicleCar.java
@@ -4,14 +4,23 @@ import javax.swing.JComponent;
 
 
 public class VehicleCar extends JComponent implements Vehicle
-{
+{	
 	public VehicleCar()
 	{
-		
+
 	}
 	
 	public void paintComponent(Graphics g)
 	{
 		g.drawRect(0, 0, 31, 31);
+	}
+	
+	public void move(int direction)
+	{
+		this.setLocation
+		(
+			this.getLocation().x + dir[direction].x,
+			this.getLocation().y + dir[direction].y
+		);
 	}
 }
