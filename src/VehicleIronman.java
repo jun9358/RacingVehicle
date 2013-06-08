879860c92f881f1893c5e0100984c5dfd2e8c726
Jun-ho, Song <jun9358@gmail.com>
2013-06-08 17:40:26 +0900

    Relocate VehicleIronman
    My left-top position is (0, 0).
    But JiMin's left-top position is (105, 200).
    So i relocate the vehicle.

--- src/VehicleIronman.java
+++ src/VehicleIronman.java
@@ -38,29 +38,29 @@ public class VehicleIronman extends JComponent implements Vehicle {
 
 		Color gold = new Color(225, 215, 0);
 		Color red = new Color(225, 0, 0);
-		Ellipse2D.Float c1 = new Ellipse2D.Float(200, 200, 50, 50);
+		Ellipse2D.Float c1 = new Ellipse2D.Float(95, 0, 50, 50);
 		g1.setColor(red);
 		g1.fill(c1);
-		Arc2D.Float c2 = new Arc2D.Float(200, 200, 50, 50, 90, -90, Arc2D.PIE);
+		Arc2D.Float c2 = new Arc2D.Float(95, 0, 50, 50, 90, -90, Arc2D.PIE);
 		g1.setColor(gold);
 		g1.fill(c2);
-		Arc2D.Float c3 = new Arc2D.Float(215, 207, 35, 35, 0, -90, Arc2D.PIE);
+		Arc2D.Float c3 = new Arc2D.Float(110, 7, 35, 35, 0, -90, Arc2D.PIE);
 		g1.setColor(gold);
 		g1.fill(c3);
-		Rectangle2D.Float c4 = new Rectangle2D.Float(230, 210, 10, 5);
+		Rectangle2D.Float c4 = new Rectangle2D.Float(125, 10, 10, 5);
 		g1.setColor(Color.CYAN);
 		g1.fill(c4);
 		g1.setStroke(new BasicStroke(1));
-		Rectangle2D.Float c5 = new Rectangle2D.Float(145, 215, 60, 25);
+		Rectangle2D.Float c5 = new Rectangle2D.Float(40, 15, 60, 25);
 		g1.setColor(red);
 		g1.fill(c5);
-		Rectangle2D.Float c6 = new Rectangle2D.Float(157, 225, 40, 10);
+		Rectangle2D.Float c6 = new Rectangle2D.Float(52, 25, 40, 10);
 		g1.setColor(gold);
 		g1.fill(c6);
-		Rectangle2D.Float c7 = new Rectangle2D.Float(105, 225, 40, 10);
+		Rectangle2D.Float c7 = new Rectangle2D.Float(0, 25, 40, 10);
 		g1.setColor(gold);
 		g1.fill(c7);
-		Rectangle2D.Float c8 = new Rectangle2D.Float(105, 222, 40, 10);
+		Rectangle2D.Float c8 = new Rectangle2D.Float(0, 22, 40, 10);
 		g1.setColor(red);
 		g1.fill(c8);
 	}
